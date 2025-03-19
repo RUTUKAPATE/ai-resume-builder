87a34b521b9873072facbfb6ad535e9165f6ccb7
itz-shantnu097 <shantnufartode.21@stvincentngp.edu.in>
2025-03-19 16:01:09 +0530

    feat: add dashboard section with UI and state management

--- app/resume/create/page.tsx
+++ app/resume/create/page.tsx
@@ -1,14 +1,18 @@
 "use client";
-import { ResumeContext } from "@/context/resume"
-import { useContext } from "react"
+import BasicInfo from "@/components/steps/basic-info";
+import BasicInfoCreate from "@/components/steps/basic-info-create";
+import StepsNav from "@/components/steps/steps-nav";
+import { ResumeContext } from "@/context/resume";
+import { useContext } from "react";
 
 const CreateResume = () => {
-    const ctx = useContext(ResumeContext);
-    return (
-        <div>
-            {ctx?.step === 1 && "step"}
-        </div>
-  );
+	const ctx = useContext(ResumeContext);
+	return (
+		<div className="flex flex-col gap-4 items-center justify-center h-screen">
+			<StepsNav />
+			{ctx?.step === 1 && <BasicInfoCreate />}
+		</div>
+	);
 };
 
-export default CreateResume
+export default CreateResume;
